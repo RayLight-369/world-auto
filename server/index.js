dfbcf60aeda49361dbcfacea3c8d2f8eb3a72cd4
Ray <buttrafay980@gmail.com>
2024-07-22 12:27:33 +0500

    update

--- server/index.js
+++ server/index.js
@@ -27,6 +27,7 @@ app.use( ( req, res, next ) => {
 app.use( bodyParser.json() );
 app.use( bodyParser.urlencoded( { extended: true } ) );
 app.use( cookieParser() );
+
 app.use( "/admin", AdminRouter );
 
 app.get( "/", ( req, res ) => res.send( "Hello, world!" ) );
