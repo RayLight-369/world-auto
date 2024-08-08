8802c5e541a6a8c9112392e112b9c988bf923519
Ray <buttrafay980@gmail.com>
2024-08-08 21:57:28 +0500

    update vtc page

--- client/src/Pages/VTCtransport/VTCtransport.jsx
+++ client/src/Pages/VTCtransport/VTCtransport.jsx
@@ -5,7 +5,9 @@ const VTCtransport = () => {
   return (
     <section id={ Styles[ "vtc" ] }>
       <div className={ Styles[ "hero" ] }>
-        <h1>Driver transport VTC</h1>
+        <h1>L'excellence VTC, à tout moment</h1>
+        <p className={ Styles[ "desc" ] }>Vivez un voyage en toute fluidité avec nos services VTC premium. Votre voyage, notre priorité.</p>
+        <a target='_blank' href={ `https://wa.me/33751287393?text=${ encodeURIComponent( `Hé, je vous ai contacté via le site WorldAuto ! Je veux parler du transport chauffeur VTC.` ) }` }>Demander un transport</a>
       </div>
     </section>
   );
